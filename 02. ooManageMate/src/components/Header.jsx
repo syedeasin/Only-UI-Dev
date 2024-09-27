626cfd2bb7266a89866f9ecdebad2328c2b3da83
syedeasin <syedeasin1000@gmail.com>
2024-09-27 11:35:57 +0600

    Logo Fixed

--- 02. ooManageMate/src/components/Header.jsx
+++ 02. ooManageMate/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <header className="bg-blue-500 text-white p-4">
+        <header className="h-16 bg-blue-500 text-white p-4">
             <h1>ooManageMate Dashboard</h1>
         </header>
     );
--- 02. ooManageMate/src/components/Sidebar.jsx
+++ 02. ooManageMate/src/components/Sidebar.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import {useState} from "react";
+import logo from '../assets/Logo/cclFullLogo.png';
+import logoIcon from '../assets/Logo/cclLogoIcon.png';
+import { AiOutlineMenu } from "react-icons/ai";
+
 
 const Sidebar = () => {
+    const [isCollapsed, setIsCollased] = useState(false);
+
+    const handleClick = () => {
+        setIsCollased(!isCollapsed);
+    }
+
     return (
-        <aside className="w-64 h-[100vh] bg-gray-800 text-white  p-4">
-            <div className="flex items-center justify-between">
-                <img src={}/>
+        <aside className={`${isCollapsed? 'w-20' : 'w-64'}  h-[100vh] bg-gray-800 text-white  px-4 transition-all duration-300`}>
+            <div className="h-16 flex items-center justify-between">
+                <img src={isCollapsed ? logoIcon : logo} alt="logo" className={`${isCollapsed ? 'w-8' : 'w-32'}`} />
+                <AiOutlineMenu onClick={handleClick} />
             </div>
             <ul>
                 <li>To-Do</li>
